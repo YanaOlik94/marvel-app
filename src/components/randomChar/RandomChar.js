22d5dfe45b5ac499bd2c94ad4c6dbfd8fd20237c
nastyaZamre <anastasiazamrekut@gmail.com>
2025-01-29 16:33:14 +0200

    Rewrite classes to functional components and hooks

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,81 +1,80 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 import Spinner from '../spinner/Spinner'
 import ErrorMessage from '../errorMessage/ErrorMessage'
 import MarvelService from "../../services/MarvelService";
 
-class RandomChar extends Component {
+const RandomChar = () => {
 
-    state = {
-        char: {},
-        loading: true,
-        error: false
+    const [char, setChar] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+
+    const marvelService = new MarvelService();
+
+
+    useEffect(() => {
+        updateChar();
+        const timerId = setInterval(updateChar, 60000);
+
+        return () => {
+            clearInterval(timerId)
+        }
+    }, [])
+
+
+    const onCharLoaded = (char) => {
+        setLoading(false);
+        setChar(char);
     }
 
-marvelService = new MarvelService();
+    const onCharLoading = () => {
+        setLoading(true);
+    }
+
+    const onError = () => {
+        setError(true);
+        setLoading(false);
+    }
 
 
-componentDidMount() {
-    this.updateChar();
-    // this.timerId = setInterval(this.updateChar, 50 00);
-}
-
-
-componentWillUnmount() {
-    clearInterval(this.timerId);
-}
-
-onCharLoaded = (char) => {
-    this.setState({char, loading: false})
-}
-
-onErrorCatch = () => {
-    this.setState({
-        loading: false,
-        error: true
-    })
-}
-
-updateChar = () => {
+const updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    this.setState({
-        loading: true
-    })
-    this.marvelService
-    .getCharacter(id)
-    .then(this.onCharLoaded)
-    .catch(this.onErrorCatch)
+    onCharLoading();
+
+    marvelService
+        .getCharacter(id)
+        .then(onCharLoaded)
+        .catch(onError);
 }
 
-render() { 
 
-    const { char, loading, error } = this.state;
-    const errorMessage = error ? <ErrorMessage/> : null; 
-    const spinner = loading ? <Spinner/> : null;
-    const content = !(loading || error) ? <View char={char}/> : null;
-  
-    return (
-        <div className="randomchar">
-            {errorMessage}
-            {spinner}
-            {content}
-            <div className="randomchar__static">
-                <p className="randomchar__title">
-                    Random character for today!<br/>
-                    Do you want to get to know him better?
-                </p>
-                <p className="randomchar__title">
-                    Or choose another one
-                </p>
-                <button className="button button__main" onClick={this.updateChar} >
-                    <div className="inner">try it</div>
-                </button>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-            </div>
+const errorMessage = error ? <ErrorMessage/> : null; 
+const spinner = loading ? <Spinner/> : null;
+const content = !(loading || error) ? <View char={char}/> : null;
+
+return (
+    <div className="randomchar">
+        {errorMessage}
+        {spinner}
+        {content}
+        <div className="randomchar__static">
+            <p className="randomchar__title">
+                Random character for today!<br/>
+                Do you want to get to know him better?
+            </p>
+            <p className="randomchar__title">
+                Or choose another one
+            </p>
+            <button className="button button__main" onClick={updateChar} >
+                <div className="inner">try it</div>
+            </button>
+            <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
         </div>
-    )
-   }
+    </div>
+)
 }
 
 

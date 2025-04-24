1c3265f9e2f484d4515d237e8537e2e391881c49
Yana Olik <yanaolik94@gmail.com>
2025-04-24 16:10:26 +0300

    seo optimization, React-helmet

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,13 +1,21 @@
+import { Helmet } from 'react-helmet';
+
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner";
 
 const ComicsPage = () => {
     return (
-        <>
-            <AppBanner/>
-            <ComicsList/>
-        </>
-    )
+      <>
+        <Helmet>
+          <meta 
+            name='description' 
+            content='Page with list of comics' />
+          <title>Comics Page</title>
+        </Helmet>
+        <AppBanner />
+        <ComicsList />
+      </>
+    );
 }
 
 export default ComicsPage;
